56eebcbdbfabde37d1de96a2958f0199dba3b503
hrishiakhade <hrishakhade@gmail.com>
2017-08-05 23:17:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/one', function (req, res) {
+  res.send(path.join(' Article One will be here in a moment'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
