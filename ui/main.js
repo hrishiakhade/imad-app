b700cedd57dec079e199bc6e0f37a283d4d719f1
hrishiakhade <hrishakhade@gmail.com>
2017-08-14 20:57:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var i=document.getElementById("Img");
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
-    i.syle.marginLeft=marginLeft + "px";
+    i.style.marginLeft=marginLeft + "px";
 }
 i.onclick=function(){
     var interval=setInterval(moveRight,100);
