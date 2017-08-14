5a3925c3b7f98965f6cc508dc16ca3346e532eb1
hrishiakhade <hrishakhade@gmail.com>
2017-08-14 22:04:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,11 @@
-console.log('Loaded!');
-
-var i=document.getElementById("Img");
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    i.style.marginLeft=marginLeft + "px";
-}
-i.onclick=function(){
-    var interval=setInterval(moveRight,50);
-  
+//Counter
+var counter=0;
+var button=document.getElementById("counter");
+button.onclick=function(){
+    
+    
+    
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML() = counter.toString();
 };
